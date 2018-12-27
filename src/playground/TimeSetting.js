7f30649471ed47c9ae232f16a4bb547f124bde70
jamal7X7 <jamal7x7@gmail.com>
2018-12-27 21:29:05 +0000

    title added

--- src/playground/TimeSetting.js
+++ src/playground/TimeSetting.js
@@ -22,3 +22,27 @@
 //   let tms = (Date.now() - begining) / 100
 //   stop ? setTime(Number.parseFloat(tms).toFixed(1)) : setTime(time)
 // })
+
+//////////////////////////////////////////////////////////////////////////
+
+// let fps = {
+// 	startTime: 0,
+// 	frameNumber: 0,
+// 	getFPS: function() {
+// 		this.frameNumber++
+// 		var d = new Date().getTime(),
+// 			currentTime = (d - this.startTime) / 1000,
+// 			result = Math.floor(this.frameNumber / currentTime)
+// 		if (currentTime > 1) {
+// 			this.startTime = new Date().getTime()
+// 			this.frameNumber = 0
+// 		}
+// 		return result
+// 	}
+// }
+
+// function gameLoop() {
+// 	setTimeout(gameLoop, 1000 / 60)
+// 	console.log(fps.getFPS())
+// }
+// window.onload = gameLoop
